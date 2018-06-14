ab2a6b4f851baa65f9d94e87eab27eb69486f081
mingyu <rlaalsrbgk@gmail.com>
2018-06-15 00:04:34 +0900

    test이후 못 푼 문제풀이

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from django.urls import path
-from .views import school_list
+
+from .views import school_list, school_detail, student_list, student_detail
 
 urlpatterns = [
-    url(r'^school/', school_list)
+    url(r'^school/$', school_list),
+    url(r'^school/(\d)+/$', school_detail),
+    url(r'^students/$', student_list),
+    url(r'^students/(\d)+/$', student_detail),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -6,6 +6,36 @@ from .models import School,Student
 def school_list(request):
     schools = School.objects.all()
 
-    return HttpResponse(schools)
+    context = {
+        'schools': schools,
+    }
+    return render(request, 'blog/school-list.html', context)
 
 
+def school_detail(request, school_id):
+
+    school = School.objects.get(id=school_id)
+    context = {
+        'school': school,
+    }
+    return render(request, 'blog/detail.html', context)
+
+
+def student_list(request):
+    students = Student.objects.all()
+
+    context = {
+        'students': students
+    }
+
+    return render(request, 'blog/student-list.html', context)
+
+
+def student_detail(request, student_id):
+    student = Student.objects.get(id= student_id)
+
+    context = {
+        'student': student
+    }
+
+    return render(request, 'blog/student-detail.html', context)
